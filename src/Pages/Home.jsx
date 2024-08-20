756998872035912edf9333c0f9a077918584045c
Neto Russo <netoacrn1@gmail.com>
2024-08-19 22:16:41 -0300

    subindo a pagina base da home

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Chamada from '../Components/Chamada';
+import MapaMG from '../Components/Mapa';
+
+
+const Home = () => {
+  return (
+    <>
+      <Chamada />
+      <MapaMG />
+    </>
+  );
+};
+
+export default Home;
