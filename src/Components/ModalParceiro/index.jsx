78186448bd8d437294970f3254db778a9bb07171
Neto Russo <netoacrn1@gmail.com>
2024-08-22 19:59:15 -0300

    Subindo o projeto intermediario

--- src/Components/ModalParceiro/index.jsx
+++ src/Components/ModalParceiro/index.jsx
@@ -27,8 +27,44 @@ const ModalParceiro = () => {
         },
       }}
     >
-      <button onClick={() => setIsOpen(false)} className={Style.close}>X</button>
       <div className={Style.modalContainer}>
+        <button onClick={() => setIsOpen(false)} className={Style.close}>X</button>
+        <div className={Style.cabeca}>
+        <h2 className={Style.titulo}>Titulo</h2>
+        <div className={Style.tags}>
+          <p className={Style.tag}>#Brinquedo</p>
+          <p className={Style.tag}>#Escolar</p>
+          <p className={Style.tag}>#Puericultura</p>
+        </div>
+        <div className={Style.conteudo}>
+
+          <div className={Style.slideContainer}>
+            <Slide
+              imagem01={img01}
+              imagem02={img02}
+              imagem03={img03}
+              imagem04={img04}
+              imagem05={img05}
+            />
+          </div>
+
+          <div className={Style.textoBox}>
+            <div className={Style.texto}>
+              <p className={Style.descricao}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec imperdiet ultrices, ante mi dapibus enim, quis imperdiet nunc nisi quis enim. Donec velit neque, dapibus nec libero a, sagittis aliquam nunc. Nullam eget nisi commodo, varius metus quis, vestibulum arcu. Morbi ut metus mauris. Aliquam erat volutpat. Nulla facilisi. Donec pulvinar quam vel risus pulvinar, sit amet elementum elit tincidunt. Maecenas ut enim erat.</p>
+            </div>
+            <div className={Style.botoes}>
+              <a className={Style.botao} href="#">Site <img className={Style.iconBtn} src={SiteIcon} alt="site" /></a>
+              <a className={Style.botao} href="#">Catalogo <img className={Style.iconBtn} src={CatalogoIcon} alt="catalogo" /></a>
+              <a className={Style.botao} href="#">Compartilhar <img className={Style.iconBtn} src={CompartilharIcon} alt="compartilhar" /></a>
+            </div>
+          </div>
+          
+        </div>
+        </div>
+
+
+      </div>
+      {/* <div className={Style.modalContainer}>
         <h2 className={Style.titulo}>Titulo</h2>
         <div className={Style.conteudo}>
           <div className={Style.slideContainer}>
@@ -49,7 +85,7 @@ const ModalParceiro = () => {
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
 
     </ReactModal>
   )
